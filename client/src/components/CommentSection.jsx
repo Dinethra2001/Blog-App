c910b8094eec27492fc9b7e04631c71e49040f49
Dinethra Sulakshana <dinethrasulakshana@gmail.com>
2024-07-05 19:31:51 +0530

    add edit functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -40,6 +40,7 @@ export default function CommentSection({ postId }) {
       setCommentError(error.message);
     }
   };
+
   useEffect(() => {
     const getComments = async () => {
       try {
@@ -81,6 +82,14 @@ export default function CommentSection({ postId }) {
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
   };
 
   return (
@@ -146,7 +155,7 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit} />
           ))}
         </>
       )}
